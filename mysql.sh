a55ea08a370fac935ce969dff406e954901605dc
Karthik <anoopkarthikaggala@gmail.com>
2024-09-10 19:59:51 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -20,7 +20,7 @@ CHECK_ROOT(){
     fi
 }
 
-VALIDATE(){uj
+VALIDATE(){
     if [ $1 -ne 0 ]
     then
         echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
@@ -43,7 +43,7 @@ VALIDATE $? "Enabled MySQL Server"
 systemctl start mysqld &>>$LOG_FILE
 VALIDATE $? "Started MySQL server"
 
-mysql -h kumarkarti.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+mysql -h kumar -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
